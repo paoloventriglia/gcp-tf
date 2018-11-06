249ded04365261271f5a118bc59138206f1bcace
paolo@corebox.co.uk <paolo@corebox.co.uk>
2018-11-06 22:14:19 +0000

    works

--- main.tf
+++ main.tf
@@ -1,30 +1,38 @@
 module "vpc" {
-    source = "module/vpc"
-    project_id   = "corebox-001"
+    source = "modules/vpc"
+    project_id = "corebox-001"
     network_name = "corebox-001-vpc"
 
     subnets = [
         {
-            subnet_name           = "public-app"
-            subnet_ip             = "10.0.1.0/24"
-            subnet_region         = "us-west1"
+            subnet_name = "public-app"
+            subnet_ip = "10.0.1.0/24"
+            subnet_region = "us-west1"
         },
         {
-            subnet_name           = "public-app"
-            subnet_ip             = "10.0.2.0/24"
-            subnet_region         = "us-west1"
+            subnet_name = "private-app"
+            subnet_ip = "10.0.2.0/24"
+            subnet_region = "us-west1"
             subnet_private_access = "true"
-            subnet_flow_logs      = "true"
+            subnet_flow_logs = "true"
         },
     ]
 
     secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "container-secondary"
-                ip_cidr_range = "172.100.0.1/24"
-            }
-        ]
+      public-app = [
+        {
+          range_name = "private-app-secondary"
+          ip_cidr_range = "192.168.1.0/24"
+        }
+      ]
+      private-app = [
+        {
+          range_name = "private-app-secondary"
+          ip_cidr_range = "192.168.2.0/24"
+        }
+      ]
 
 
     }
+
+}
