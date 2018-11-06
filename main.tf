7d6e83b6d7cabfa8e98b17270097ef8de6f8c324
paolo@corebox.co.uk <paolo@corebox.co.uk>
2018-11-06 21:46:10 +0000

    changed vpc module

--- main.tf
+++ main.tf
@@ -1,6 +1,30 @@
-module "network" {
-  source = "modules/network"
-  name = "myvpc"
-}
+module "vpc" {
+    source = "module/vpc"
+    project_id   = "corebox-001"
+    network_name = "corebox-001-vpc"
+
+    subnets = [
+        {
+            subnet_name           = "public-app"
+            subnet_ip             = "10.0.1.0/24"
+            subnet_region         = "us-west1"
+        },
+        {
+            subnet_name           = "public-app"
+            subnet_ip             = "10.0.2.0/24"
+            subnet_region         = "us-west1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+        },
+    ]
+
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "container-secondary"
+                ip_cidr_range = "172.100.0.1/24"
+            }
+        ]
 
 
+    }
--- modules/network/main.tf
+++
@@ -1,5 +0,0 @@
-resource "google_compute_network" "vpc" {
-  name                    = "${var.name}"
-  auto_create_subnetworks = "${var.auto_create_subnetworks}"
-  description             = "Will create a VPC with subnet for each region created automatically"
-}
