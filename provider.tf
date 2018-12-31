32698cd49a1d97d71eb577a1c5c80d8527a7ae78
paolo <paolo@corebox.co.uk>
2018-12-31 15:52:34 +0000

    Moved module to separate repo, created vpc folder

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   credentials = "${file("tf-account-cred.json")}"
   project     = "corebox-001"
-  region      = "europe-west2"
+  region      = "us-central2"
 }
---
+++ vpc/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+    source = "modules/vpc"
+    project_id = "${var.project_id}"
+    network_name = "${var.network_name}"
+
+    subnets = "${var.subnets}"
+
+    secondary_ranges = "${var.secondary_ranges}"
+
+
+    }
