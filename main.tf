4464b8f71be9138c2f922e85dae0161564d92d3b
paolo <paolo@corebox.co.uk>
2018-12-31 15:57:39 +0000

    removed files

--- main.tf
+++
@@ -1,7 +0,0 @@
-module "vpc" {
-    source = "modules/vpc"
-    project_id = "${var.project_id}"
-    network_name = "${var.network_name}"
-    subnets = "${var.subnets}"
-    secondary_ranges = "${var.secondary_ranges}"
-}
